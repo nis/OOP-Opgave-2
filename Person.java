9bb1e4a5aa45574b008c72d3c38c628ea264df3b
Nis Sarup <nis@sarup.dk>
2010-10-14 15:41:08 +0200

    Added the Person class.

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+	public String pNavn;
+	public String pNat;
+	public int pBorn;
+	public int pDead;
+	
+	public Person(String navn, String nat, int f, int d) {
+		pNavn = navn;
+		pNat = nat;
+		pBorn = f;
+		pDead = d; 
+	}
+	
+}
