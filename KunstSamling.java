e2aa2e191461defad0876c2b416f646e3c7edaf1
Nis Sarup <nis@sarup.dk>
2010-10-15 16:19:08 +0200

    Added the last two questions. Everything runs smoothly now.

--- KunstSamling.java
+++ KunstSamling.java
@@ -1,12 +1,38 @@
 public class KunstSamling {
 	public Kunst[] kunstværker = new Kunst[7];
 	
+	public void beregnAntalLæsteSider () {
+		System.out.println("Antal læste sider:");
+		int s = 0;
+		for (int i = 0; i < 7; i++) {
+			if (kunstværker[i].getClass().getName() == "Bog") {
+				Bog t = (Bog) kunstværker[i];
+				if (t.antalSider > 0) {
+					s += t.antalSider;
+				}
+			}
+		}
+		System.out.println("\t" + s + " sider");
+	}
+	
+	public void findMalerierPåMuseum(String m) {
+		System.out.println("Malerier på museet \"" + m + "\":");
+		for (int i = 0; i < 7; i++) {
+			if (kunstværker[i].getClass().getName() == "Maleri") {
+				Maleri t = (Maleri) kunstværker[i];
+				if (t.museum.mNavn == m) {
+					System.out.println("\t" + kunstværker[i].titel);
+				}
+			}
+		}
+	}
+	
 	public void findSangeMedFlereKomponister () {
 		System.out.println("Sange med flere komponister:");
 		for (int i = 0; i < 7; i++) {
 			if (kunstværker[i].getClass().getName() == "Komposition") {
 				if (kunstværker[i].numOphavsmænd() > 1) {
-					System.out.println("Titel: " + kunstværker[i].titel);
+					System.out.println("\t" + kunstværker[i].titel);
 				}
 			}
 		}
