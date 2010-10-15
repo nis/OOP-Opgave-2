4b09daa3b1e63f8f26e2dc3aeb5217f28b3aaafe
Nis Sarup <nis@sarup.dk>
2010-10-15 12:27:50 +0200

    Added the method to find music with more than one ophavsmand.

--- KunstSamling.java
+++ KunstSamling.java
@@ -1,6 +1,17 @@
 public class KunstSamling {
 	public Kunst[] kunstværker = new Kunst[7];
 	
+	public void findSangeMedFlereKomponister () {
+		System.out.println("Sange med flere komponister:");
+		for (int i = 0; i < 7; i++) {
+			if (kunstværker[i].getClass().getName() == "Komposition") {
+				if (kunstværker[i].numOphavsmænd() > 1) {
+					System.out.println("Titel: " + kunstværker[i].titel);
+				}
+			}
+		}
+	}
+	
 	public void add(Kunst k) {
 		// Find en ledig plads
 		for (int i = 0; i < 7; i++) {
