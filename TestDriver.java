1f44d7bb22882c743264f8f2d2ed47db6f344f86
Nis Sarup <nis@sarup.dk>
2010-10-15 16:19:38 +0200

    Added the three methods from the end of the assignment.

--- TestDriver.java
+++ TestDriver.java
@@ -8,7 +8,7 @@ public class TestDriver
         Person lar = new Person ("L.A. Ring","dansk",1854,1933);
         Maleri kal = new Maleri ("Kalkmanden",1908,80,65,smk); 
         kal.addOphavsmand(lar);
-
+		
         Person cwe = new Person ("C. W. Eckersberg","dansk",1783,1853);
         Maleri rus = new Maleri ("Et russisk linjeskib til ankers ved Helsingør",1828,140,135,fsm);
         rus.addOphavsmand(cwe);
@@ -21,16 +21,12 @@ public class TestDriver
         kvs.add(kal);
         kvs.add(rus);
         kvs.add(par);
-        
-		//kvs.printMe();
 
         Person jl = new Person ("John Lennon","britisk",1940,1980);
         Person pm = new Person ("Paul Mccartney","britisk",1942,0);
         Komposition y = new Komposition("Yesterday",1965,"Rock",211);
         y.addOphavsmand(jl);
         y.addOphavsmand(pm);
-        
-		//y.printMe();
 
         Person db = new Person ("David Bowie","britisk",1947,0);
         Komposition lom = new Komposition("Life on Mars",1971,"Rock",309);
@@ -42,8 +38,6 @@ public class TestDriver
         Person mah = new Person ("Martin A. Hansen","dansk",1909,1955);
         Bog l = new Bog("Løgneren",1950,"Prosa",232);
         l.addOphavsmand(mah);
-
-		//l.printMe();
          
         Person hs = new Person ("Hans Scherfig","dansk",1905,1978);
         Bog dff = new Bog("Det forsømte Forår",1940,"Prosa",218);
@@ -52,6 +46,12 @@ public class TestDriver
         kvs.add(l);
         kvs.add(dff);
 
-    	kvs.printMe();
+		kvs.findSangeMedFlereKomponister();
+		System.out.println("");
+		
+		kvs.findMalerierPåMuseum("Statens Museum for Kunst");
+		System.out.println("");
+		
+		kvs.beregnAntalLæsteSider();
     }
 }
